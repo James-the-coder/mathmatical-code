007849f0b8c8b953784ab97a769e467668cc5a14
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2019-11-07 20:20:53 +0000

    Add files via upload
    calculates mean and median from a set of numbers

---
+++ mean median.py
@@ -0,0 +1,36 @@
+
+
+
+numList = []
+total = 0
+running = True
+
+def median(n):
+    if len(n) % 2 == 0:
+        mid = len(n) // 2
+    else:
+        mid = (len(n)+1)//2
+    return n[mid-1]
+
+def mean(x, n):
+    if len(n) % 2 == 0:
+        mean = x // len(n)
+    else:
+        mean = x // len(n)+1
+
+    return mean
+
+
+
+while running:
+    loops = int(input("How many numbers do you want to enter?: "))
+    for i in range(loops):
+        num = int(input("Enter a number: "))
+        total += num
+        numList.append(num)
+
+    print("The median is "+str(median(numList)))
+    print("The mean is "+str(mean(total, numList)))
+    
+    
+    
