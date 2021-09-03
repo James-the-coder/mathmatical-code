a9de5905d556607f2c6d38bed372f16ae472a9a3
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2021-09-03 14:19:03 +0100

    Update Factorising.cs

--- Factorising.cs
+++ Factorising.cs
@@ -9,15 +9,15 @@ namespace Factorisor
         {
             List<int> numbers = new List<int>();
             Console.WriteLine("Linear Equation Factorising");
-            Console.WriteLine("Enter the equation: ");
+            Console.WriteLine("Enter the equation: "); //in the form ax+b
             string equation = Console.ReadLine();
             numbers = LocateNumbers(equation);
             int a = numbers[0];
             int b = numbers[1];
             int result  = HighestCommonFactor(a,b);
-            Console.WriteLine($"{result}({a/result}x + {b/result})");
+            Console.WriteLine($"{result}({a/result}x + {b/result})"); //returns a string in the form highestCommonFactor(ax+b)
         }
-
+        // function that returns the numbers from the equation
         static List<int> LocateNumbers(string eq)
         {
             List<int> numbers_ = new List<int>();
@@ -40,7 +40,7 @@ namespace Factorisor
             numbers_.Add(Convert.ToInt32(num));
             return numbers_;
         }
-
+        //returns the highest common factor of both numbers
         static int HighestCommonFactor(int a, int b)
         {
             
