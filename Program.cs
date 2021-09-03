c6338a6a921c6385a972ea3822fcd52a0b070c80
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2021-09-03 14:27:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace factorial_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                BigInteger factorial = 1;
+                int total = 0;
+                Console.WriteLine("Enter the number: ");
+                int num = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+                string number = Convert.ToString(factorial);
+                char[] digits = number.ToCharArray();
+                foreach (char digit in digits)
+                {
+                    string stringDigit = Convert.ToString(digit);
+                    int intDigit = Convert.ToInt32(stringDigit);
+                    total += intDigit;
+                }
+                Console.WriteLine($"{num} factorial is {factorial}");
+                Console.WriteLine($"The sum of the digits is {total}");
+            }
+            
+        }
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CargoShip
+{
+    class Ship
+    {
+        public const int CycleWeight = 3;
+        public const int CarWeight = 5;
+        public const int TrainWeight = 7;
+        public const int LorryWeight = 11;
+        public const int MaxWeight = 10;
+
+        public int Capacity { get; set; }
+        public int CycleCount { get; set; }
+        public int CarCount { get; set; }
+        public int TrainCount { get; set; }
+        public int LorryCount { get; set; }
+
+        Random random = new Random();
+
+        public int getShipLoad()
+        {
+            return CarCount * CarWeight + CycleCount * CycleWeight + LorryCount * LorryWeight + TrainCount * TrainWeight;
+        }
+
+        public Ship()
+        {
+            CycleCount = 0;
+            LorryCount = 0;
+            TrainCount = 0;
+            CarCount = 0;
+
+            Capacity = random.Next(MaxWeight) * CycleWeight + random.Next(MaxWeight) * CarWeight + random.Next(MaxWeight) * LorryWeight + random.Next(MaxWeight) * TrainWeight;
+        }
+
+        public int overUnder()
+        {
+            return Capacity - getShipLoad();
+        }
+
+        public override string ToString()
+        {
+            return ($"Capacity = {Capacity}, CurrentLoad = {getShipLoad()}");
+        }
+
+    }
+}
